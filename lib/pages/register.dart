713c3659fba4f6f021ee58e4fc52d2bd97dff8db
Chaiwat Tungtongsoontorn <chaiwat_t@tripetch-it.co.th>
2023-03-06 22:33:48 +0700

    feat: registerable app

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import '../widgets/login_form.dart';
 
@@ -10,12 +13,31 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  void onFormLoginSubmit(String username, String password) {
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
+
+  Future<void> onFormLoginSubmit(String username, String password) async {
     print("================Register[onFormLoginSubmit]================");
     print('text field: ${username}');
     print('-----');
     print('text field: ${password}');
     print("=================================");
+
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: username, password: password);
+    } on FirebaseAuthException catch (e) {
+      final String errorMsg = e.toString();
+
+      print(errorMsg);
+      Fluttertoast.showToast(
+          msg: errorMsg,
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
   }
 
   void onTextBtnClick(BuildContext context) {
@@ -24,29 +46,57 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Flex(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        direction: Axis.vertical,
-        children: [
-          Center(
-            child: Column(
-              children: [
-                Container(
-                    margin: const EdgeInsets.only(left: 20, right: 20),
-                    child: LoginForm(
-                      btnTitle: "Register",
-                      onSubmit: onFormLoginSubmit,
-                    )),
-                TextButton(
-                    onPressed: () => onTextBtnClick(context),
-                    child: const Text('Back to Home'))
-              ],
+    return FutureBuilder(
+        future: firebase,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text("Error"),
+              ),
+              body: Flex(
+                direction: Axis.vertical,
+                children: [
+                  Center(
+                    child: Text(snapshot.error.toString()),
+                  )
+                ],
+              ),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              body: Flex(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                direction: Axis.vertical,
+                children: [
+                  Center(
+                    child: Column(
+                      children: [
+                        Container(
+                            margin: const EdgeInsets.only(left: 20, right: 20),
+                            child: LoginForm(
+                              btnTitle: "Register",
+                              onSubmit: onFormLoginSubmit,
+                            )),
+                        TextButton(
+                            onPressed: () => onTextBtnClick(context),
+                            child: const Text('Back to Home'))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-          )
-        ],
-      ),
-    );
+          );
+        });
   }
 }
